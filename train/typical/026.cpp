a67d6698a2481eb449df275ce2a3900eab279238
enjoy82 <naoya990506@gmail.com>
2021-04-28 20:42:12 +0900

    easy

---
+++ train/typical/026.cpp
@@ -0,0 +1,122 @@
+// execute g++ main.cpp -std=c++14 -I C:\Users\naoya\Desktop\code\Atcoder
+#include<bits/stdc++.h>
+//#include<atcoder/all>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+//using namespace atcoder;
+
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+
+//ordered_set 重複不可
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// use set_function + find_by_order(select itr-num)
+
+#define REP(i, l, n) for(int i=(l), i##_len=(n); i<i##_len; ++i)
+#define ALL(x) (x).begin(),(x).end()
+#define pb push_back
+
+ll gcd(ll a,ll b){return b ? gcd(b,a%b) : a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+template<class T>inline bool chmax(T &a, T b) {if(a < b) {a = b;return true;}return false;}
+template<class T>inline bool chmin(T &a, T b) {if(a > b) {a = b;return true;}return false;}
+
+
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+//cout << std::fixed << std::setprecision(15) << y << endl; //小数表示
+
+int n;
+
+int vcount(vector<int> &flag){
+    int count = 0;
+    REP(i,0,flag.size()){
+        if(flag[i] == 1){
+            count++;
+        }
+    }
+    return count;
+}
+
+void outans(vector<int> &flag){
+    int count = 0;
+    REP(i,0,flag.size()){
+        if(flag[i] == 1){
+            cout << i+1 << " ";
+            count++;
+            if(count == n/2)
+                break;
+        }
+    }
+    cout  << endl;
+    return;
+}
+
+int main(){
+    cin >> n;
+    vector<vector<int> > tree(n);
+    REP(i,0,n-1){
+        int a, b; cin >> a >> b;
+        a--; b--;
+        tree[a].pb(b);
+        tree[b].pb(a);
+    }
+
+    //first color = 1
+    vector<int> flag(n, 0); // 1 & -1
+    flag[0] = 1;
+    queue<Pii> que;
+    que.push(Pii(0, 1));
+    while(!que.empty()){
+        Pii now = que.front();
+        que.pop();
+        int pos = now.first, pre = now.second;
+        int color = pre * -1;
+        REP(i,0,tree[pos].size()){
+            int next = tree[pos][i];
+            if(flag[next] == 0){
+                flag[next] = color;
+                que.push(Pii(next, color));
+            }else{
+                continue;
+            }
+        }
+    }
+    if(vcount(flag) >= n / 2){
+        outans(flag);
+        return 0;
+    }
+
+    //first color = -1
+    flag = vector<int>(n, 0); // 1 & -1
+    flag[0] = -1;
+    que.push(Pii(0, -1));
+    while(!que.empty()){
+        Pii now = que.front();
+        que.pop();
+        int pos = now.first, pre = now.second;
+        int color = pre * -1;
+        REP(i,0,tree[pos].size()){
+            int next = tree[pos][i];
+            if(flag[next] == 0){
+                flag[next] = color;
+                que.push(Pii(next, color));
+            }else{
+                continue;
+            }
+        }
+    }
+    if(vcount(flag) >= n / 2){
+        outans(flag);
+        return 0;
+    }
+}
