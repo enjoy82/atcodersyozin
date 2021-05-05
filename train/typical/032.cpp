30ff45f725945cde3c9c2192125dd4f2d8936f98
enjoy82 <naoya990506@gmail.com>
2021-05-05 23:58:09 +0900

    add

---
+++ train/typical/032.cpp
@@ -0,0 +1,82 @@
+// execute g++ main.cpp -std=c++14 -I C:\Users\naoya\Desktop\code\Atcoder
+#include<bits/stdc++.h>
+//#include<atcoder/all>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+//using namespace atcoder;
+
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+
+//ordered_set 重複不可
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// use set_function + find_by_order(select itr-num)
+
+#define REP(i, l, n) for(int i=(l), i##_len=(n); i<i##_len; ++i)
+#define ALL(x) (x).begin(),(x).end()
+#define pb push_back
+
+ll gcd(ll a,ll b){return b ? gcd(b,a%b) : a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+template<class T>inline bool chmax(T &a, T b) {if(a < b) {a = b;return true;}return false;}
+template<class T>inline bool chmin(T &a, T b) {if(a > b) {a = b;return true;}return false;}
+
+
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+//cout << std::fixed << std::setprecision(15) << y << endl; //小数表示
+
+const ll inf = 1e18;
+
+int main(){
+    int n; cin >> n;
+    vector<vector<ll> > lis(n, vector<ll>(n));
+    REP(i,0,n){
+        REP(l,0,n){
+            cin >> lis[i][l];
+        }
+    }
+    int m; cin >> m;
+    map<Pii, int> mp;
+    REP(i,0,m){
+        int a, b; cin >> a >> b;
+        a--; b--;
+        mp[Pii(a, b)] = 1;
+        mp[Pii(b, a)] = 1;
+    }
+    vector<vector<ll> > dp(1<<n, vector<ll>(n, inf));
+    REP(i,0,n){
+        dp[1 << i][i] = lis[i][0];
+    }
+    REP(i, 1, (1<<n)){
+        REP(l,0,n){
+            if(dp[i][l] < inf){
+                REP(k,0,n){
+                    if((i & (1 << k)) == 0){
+                        if(mp[Pii(l,k)] == 1) continue;
+                        int c = __builtin_popcount(i);
+                        dp[i + (1<<k)][k] = min(dp[i][l] + lis[k][c], dp[i + (1<<k)][k]);
+                    }
+                }
+            }
+        }
+    }
+    ll ans = inf;
+    REP(i,0,n){
+        chmin(ans, dp[(1<<n)-1][i]);
+    }
+    if(ans >= inf){
+        cout << -1 << endl;
+    }else{
+        cout << ans << endl;
+    }
+}
