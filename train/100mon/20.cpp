6f60f180c5dedfbd98fcbfdf9f9c4fb04cdf13a0
enjoy82 <naoya990506@gmail.com>
2021-05-26 23:01:50 +0900

    100

---
+++ train/100mon/20.cpp
@@ -0,0 +1,61 @@
+// execute g++ main.cpp -std=c++14 -I C:\Users\naoya\Desktop\code\Atcoder
+#include<bits/stdc++.h>
+//#include<atcoder/all>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+//using namespace atcoder;
+
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+
+//ordered_set 重複不可
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// use set_function + find_by_order(select itr-num)
+
+#define REP(i, l, n) for(int i=(l), i##_len=(n); i<i##_len; ++i)
+#define ALL(x) (x).begin(),(x).end()
+#define pb push_back
+
+ll gcd(ll a,ll b){return b ? gcd(b,a%b) : a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+template<class T>inline bool chmax(T &a, T b) {if(a < b) {a = b;return true;}return false;}
+template<class T>inline bool chmin(T &a, T b) {if(a > b) {a = b;return true;}return false;}
+
+
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+//cout << std::fixed << std::setprecision(15) << y << endl; //小数表示
+
+int main(){
+    int n; cin >> n;
+    vector<ll> alis(n), blis(n), clis(n);
+    REP(i,0,n){cin >> alis[i];}
+    REP(i,0,n){cin >> blis[i];}
+    REP(i,0,n){cin >> clis[i];}
+    sort(ALL(alis));
+    sort(ALL(blis));
+    sort(ALL(clis));
+    ll ans = 0;
+    REP(i,0,n){
+        //a
+        auto it = lower_bound(ALL(alis), blis[i]);
+        it--;
+        ll dis = it - alis.begin() + 1;
+        ll mid = max(dis, 0ll);
+        //b
+        auto it2 = upper_bound(ALL(clis), blis[i]);
+        ll mid2 = clis.end() - it2;
+        //cout << *it << " " << *it2 << " " << mid << " " << mid2 << endl;
+        ans += mid * mid2;
+    }
+    cout << ans << endl;
+}
---
+++ train/100mon/21.cpp
@@ -0,0 +1,73 @@
+// execute g++ main.cpp -std=c++14 -I C:\Users\naoya\Desktop\code\Atcoder
+#include<bits/stdc++.h>
+//#include<atcoder/all>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+//using namespace atcoder;
+
+using Pii = pair<int, int>;
+using Pll = pair<ll, ll>;
+
+//ordered_set 重複不可
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// use set_function + find_by_order(select itr-num)
+
+#define REP(i, l, n) for(int i=(l), i##_len=(n); i<i##_len; ++i)
+#define ALL(x) (x).begin(),(x).end()
+#define pb push_back
+
+ll gcd(ll a,ll b){return b ? gcd(b,a%b) : a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+
+template<class T>inline bool chmax(T &a, T b) {if(a < b) {a = b;return true;}return false;}
+template<class T>inline bool chmin(T &a, T b) {if(a > b) {a = b;return true;}return false;}
+
+
+char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char Alpha[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+//cout << std::fixed << std::setprecision(15) << y << endl; //小数表示
+
+int main(){
+    int n; cin >> n;
+    vector<Pll> lis(n);
+    REP(i,0,n){
+        ll a, b; cin >> a >> b;
+        lis[i] = Pll(a, b);
+    }
+    ll left = 0, right = 1e18;
+    while(right - left > 1){
+        ll mid = (right + left) / 2;
+        vector<ll> dis(n);
+        int f = 1;
+        REP(i,0,n){
+            if(mid - lis[i].first < 0){
+                f = 0;
+            }
+            dis[i] =  (mid - lis[i].first) / lis[i].second;
+        }
+        sort(ALL(dis));
+        int pre = -1;
+        REP(i,0,n){
+            if(dis[i] <= pre){
+                f = 0;
+                break;
+            }
+            pre = i;
+        }
+        if(f == 1){
+            right = mid;
+        }else{
+            left = mid;
+        }
+    }
+    cout << right << endl;
+    return 0;
+}
